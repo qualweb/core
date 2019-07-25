37306249d6e87fc118a2914df0167383a89d97af
João Afonso Vicente <joao.vicente@campus.ul.pt>
2019-07-25 14:46:51 +0100

    Added options to module act-rules. Support for aggregated reports. Code optimizations

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@
 'use strict';
 
 import _ from 'lodash';
+import { readFile } from 'fs-extra';
+import crypto from 'crypto';
 
 import parse_url from './lib/parse_url';
 
@@ -23,16 +25,16 @@ import {
 
 import { getDom } from '@qualweb/get-dom-puppeteer';
 import { executeWappalyzer } from '@qualweb/wappalyzer';
-import { executeACTR } from '@qualweb/act-rules';
+import { configure, executeACTR } from '@qualweb/act-rules';
 
-let globalReport: EvaluationReport | undefined;
+let globalReports: EvaluationReport[] | undefined;
 
-async function evaluate(options: QualwebOptions): Promise<EvaluationReport | EvaluationReport[]> {
-  if (!options.url /*&& !options.urls && !options.file*/) {
-    throw new Error('Invalid input method');
+async function run(url: string, options: QualwebOptions): Promise<EvaluationReport> {
+  const html = await getDom(url);
+
+  if (options['act-rules']) {
+    configure(options['act-rules']);
   }
-
-  const html = await getDom(options.url);
 
   const report: EvaluationReport = {
     type: 'evaluation',
@@ -41,27 +43,61 @@ async function evaluate(options: QualwebOptions): Promise<EvaluationReport | Eva
       description: 'QualWeb is an automatic accessibility evaluator for webpages.',
       version: '3.0.0',
       homepage: 'http://www.qualweb.di.fc.ul.pt/',
-      date: new Date().toISOString(),
-      hash: '413cc48ce6e71792bcfcd99c4323df17ee577bd67a768820a0fcc01a98de7c13',
-      url: parse_url(options.url)
+      date: new Date().toLocaleDateString('pt-PT'),
+      hash: crypto.randomBytes(20).toString('hex'),
+      url: parse_url(url)
     },
     modules: {
-      wappalyzer: options.wappalyzer ? await executeWappalyzer(options.url): undefined,
+      wappalyzer: options.wappalyzer ? await executeWappalyzer(url): undefined,
       'act-rules': await executeACTR(html.parsedSourceHTML, html.parsedProcessedHTML)
     }
   };
 
-  globalReport = _.cloneDeep(report);
-
   return report;
 }
 
+async function evaluate(options: QualwebOptions): Promise<EvaluationReport | EvaluationReport[]> {
+  if (!options.url && !options.file) {
+    throw new Error('Invalid input method');
+  }
+
+  globalReports = new Array<EvaluationReport>();
+
+  if (options.url) {
+    const report = await run(options.url, options);
+    globalReports.push(_.cloneDeep(report));
+
+    return report;
+  } else if (options.file) {
+    const file = await readFile(options.file);
+    const urls = file.toString().split('\n').map(u => decodeURIComponent(u).trim());
+
+    for (const url of urls || []) {
+      const report = await run(url, options);
+      globalReports.push(_.cloneDeep(report));
+    }
+
+    return _.cloneDeep(globalReports);
+  } else {
+    throw new Error('Invalid input method');
+  }
+}
+
 async function generateEarlReport(options?: EarlOptions): Promise<EarlReport | EarlReport[] | null> {
-  if (globalReport) {
-    if (options && options.multi) {
-      return await generateAggregatedEarlReport([globalReport]);
+  if (globalReports && globalReports.length > 0) {
+    if (options && options.aggregated) {
+      return await generateAggregatedEarlReport(<EvaluationReport[]> globalReports);
     } else {
-      return await generateSingleEarlReport(globalReport);
+      if (globalReports.length === 1) {
+        return await generateSingleEarlReport(<EvaluationReport> globalReports[0]);
+      } else {
+        const earlReports = new Array<EarlReport>();
+        for (const report of globalReports || []) {
+          const earlReport = await generateSingleEarlReport(report);
+          earlReports.push(_.cloneDeep(earlReport));
+        }
+        return _.cloneDeep(earlReports);
+      }
     }
   }
 
