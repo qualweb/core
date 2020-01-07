ac1dac7c10b715eaeeb3d39d5cc09a4b36b7e9d6
João Vicente <joao.vicente@campus.ul.pt>
2020-01-07 11:36:07 +0000

    updated act-rules modules. Fixed some bugs

--- src/lib/data/evaluation.object.ts
+++ src/lib/data/evaluation.object.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import clone from 'lodash.clone';
+import cloneDeep from 'lodash.clonedeep';
 import { EvaluationReport } from '@qualweb/core';
 import Metadata from './metadata.object';
 
@@ -19,12 +20,12 @@ class Evaluation {
   }
 
   public addModuleEvaluation(module: any, evaluation: any): void {
-    this.modules[module] = clone(evaluation);
+    this.modules[module] = cloneDeep(evaluation);
     if (module !== 'wappalyzer') {
-      this.metadata.addPassedResults(evaluation.metadata.passed || 0);
-      this.metadata.addWarningResults(evaluation.metadata.warning || 0);
-      this.metadata.addFailedResults(evaluation.metadata.failed || 0);
-      this.metadata.addInapplicableResults(evaluation.metadata.inapplicable || 0);
+      this.metadata.addPassedResults(this.modules[module].metadata.passed || 0);
+      this.metadata.addWarningResults(this.modules[module].metadata.warning || 0);
+      this.metadata.addFailedResults(this.modules[module].metadata.failed || 0);
+      this.metadata.addInapplicableResults(this.modules[module].metadata.inapplicable || 0);
     }
   }
 
