171fe3159779b95f64787db590a00517b1d0ec3c
João Vicente <joao.vicente@campus.ul.pt>
2020-01-22 15:12:29 +0000

    updated act-rules module

--- test/core.spec.js
+++ test/core.spec.js
@@ -55,7 +55,7 @@ describe.only('Testing new architecture', function() {
     ];
 
     //const reports = await core.evaluate({ urls: [URL3, URL2] , execute: { act: true }, 'act-rules': { rules: ['QW-ACT-R1'] }, maxParallelEvaluations: 2});
-    const reports = await core.evaluate({ url: urls[7], execute: { act: true }, maxParallelEvaluations: 1, 'act-rules': { rules: ['QW-ACT-R35'] } });
+    const reports = await core.evaluate({ url: urls[7], execute: { act: true }, maxParallelEvaluations: 1 });
     console.log(reports);
     const earlReports = await core.generateEarlReport();
     console.log(JSON.stringify(earlReports, null, 2));
