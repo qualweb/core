fe8f4653f8063b9988db8ac6c5746824f9450885
Carlos Duarte <carlosapaduarte@gmail.com>
2021-02-08 10:00:19 +0000

    remove debug

--- test/core.spec.js
+++ test/core.spec.js
@@ -9,15 +9,11 @@ describe('Core', function () {
 
     await qualweb.start();
 
-    const evaluations = await qualweb.evaluate({ url: 'https://www.ulisboa.pt/', execute: { act: true } });
+    const evaluations = await qualweb.evaluate({ url: 'https://ciencias.ulisboa.pt/' });
 
     const earlReports = await generateEarlReport(evaluations);
 
     await qualweb.stop();
-
-    console.log(evaluations['https://www.ulisboa.pt/'].modules['act-rules'].assertions['QW-ACT-R45']);
-    //console.log(earlReports['https://www.ulisboa.pt/']['@graph'][0].assertions);
-    //console.log(evaluations.modules['act-rules'].assertions['QW-ACT-R45']);
 
     expect(earlReports['https://www.ulisboa.pt/']['@graph'].length).to.be.equal(1);
   });
